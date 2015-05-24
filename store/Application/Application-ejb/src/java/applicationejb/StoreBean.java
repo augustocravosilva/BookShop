10d1c34751c294b38cbf8b5eac05874662c2e1a7
Augusto Cravo Silva <augustocravosilva@gmail.com>
2015-05-24 22:59:51 +0100

    store services and gui structure

--- store/Application/Application-ejb/src/java/applicationejb/StoreBean.java
+++ store/Application/Application-ejb/src/java/applicationejb/StoreBean.java
@@ -5,16 +5,64 @@
  */
 package applicationejb;
 
+import javax.annotation.Resource;
 import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.jms.JMSConnectionFactory;
+import javax.jms.JMSContext;
+import javax.jms.Queue;
 
 /**
  *
  * @author augusto
  */
 @Stateless
-public class StoreBean implements StoreBeanRemote, StoreBeanLocal {
+public class StoreBean implements StoreBeanRemote {
+    @Resource(mappedName = "jms/EAppQueue")
+    private Queue eAppQueue;
+    @Inject
+    @JMSConnectionFactory("java:comp/DefaultJMSConnectionFactory")
+    private JMSContext context;
+
+    @Override
+    public void orderBook(String isbn, int quatity, int clientId) {
+        System.out.println("It was ordered.");
+        
+    }
+    
+    
+    
+    //nova ordem de compra
+
+    //public void orderBook(String isbn, int quantity, int clientId) {
+    //}
 
     // Add business logic below. (Right-click in editor and choose
     // "Insert Code > Add Business Method")
+    
+    //livros - titulos, preco, existencias loja e armazem
+    
+    
+    //venda
+    
+    //receipt
+    
+    //send email
+    
+    //request warehouse for 10x quantity
+    
+    //orderbeingshipped (Warehouse->Store)
+    
+    //update store stock
+
+    @Override
+    public String businessMethod() {
+        return "hello!";
+    }
+
+    private void sendJMSMessageToEAppQueue(String messageData) {
+        context.createProducer().send(eAppQueue, messageData);
+    }
+    
     
 }
--- store/Application/dist/gfdeploy/Application/Application-war_war/partials/faq/faq.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('myApp.faq', ['ngRoute'])
-
-    .config(['$routeProvider', function($routeProvider) {
-        $routeProvider.when('/faq', {
-            templateUrl: 'partials/faq/faq.html'
-        });
-    }]);
