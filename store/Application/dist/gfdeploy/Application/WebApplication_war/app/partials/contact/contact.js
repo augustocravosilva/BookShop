7f2ad143c0fa0b52ca44c7dc3c7fc17292db864d
Augusto Cravo Silva <augustocravosilva@gmail.com>
2015-05-27 16:21:04 +0100

    removed all build files by tiagoaf5 :)

--- store/Application/dist/gfdeploy/Application/WebApplication_war/app/partials/contact/contact.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('myApp.contact', ['ngRoute'])
-
-    .config(['$routeProvider', function($routeProvider) {
-        $routeProvider.when('/contact', {
-            templateUrl: 'partials/contact/contact.html'
-        });
-    }]);
